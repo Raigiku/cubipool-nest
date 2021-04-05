52939b277cfecc7a2d7bffaa99017578f02d4bec
Raigiku <raigiku@hotmail.com>
2021-04-05 00:47:41 -0500

    Add swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { UserTypeOrm } from "src/entities/typeorm";
 import { LoginUserStory } from "./login.user-story";
 
+@ApiTags('auth')
 @Controller("auth")
 export class AuthController {
-  constructor(private loginUserStory: LoginUserStory) {}
+  constructor(private readonly loginUserStory: LoginUserStory) {}
 
   @Get()
   testHello(): string {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Cubipool API")
+    .setDescription("This is the API for Cubipool")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
