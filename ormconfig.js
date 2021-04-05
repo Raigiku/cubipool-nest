fd745ba918744cd416ade1e896050cd37c2adb10
Raigiku <raigiku@hotmail.com>
2021-04-04 22:17:46 -0500

    Change env var name and add ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,14 @@
 module.exports = {
   type: "postgres",
-  url: process.env.POSTGRESQL_URL,
+  url: process.env.CUBIPOOL_POSTGRESQL_URL,
   synchronize: true,
   logging: true,
   timezone: "+0",
   entities: ["dist/**/*.typeorm.js"],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
