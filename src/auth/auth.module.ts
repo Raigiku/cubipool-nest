6b8b17d1f0c5adf3acf7269e5715a8ebcfdd97c8
Raigiku <raigiku@hotmail.com>
2021-04-14 22:14:16 -0500

    Remove unused import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
 import { EntitiesModule } from "src/entities/entities.module";
 import { AuthController } from "./auth.controller";
 import { LoginUserStory } from "./login";
@@ -10,7 +9,6 @@ import { JwtStrategy } from "../common/strategies/jwt.strategy";
 @Module({
   imports: [
     EntitiesModule,
-    PassportModule,
     JwtModule.register({
       secret: process.env.CUBIPOOL_JWT_SECRET,
       signOptions: { expiresIn: "7d" },
